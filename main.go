50b64c7a49ff3620c91e84856f2a8f86494ca483
jo-me <jochen.mehlhorn@gmail.com>
2020-11-25 20:19:26 +0100

    Fix behavior when containeris already up Code cleanup

--- main.go
+++ main.go
@@ -14,9 +14,6 @@ import (
 	"github.com/docker/docker/client"
 )
 
-const oneReplica = uint64(1)
-const zeroReplica = uint64(0)
-
 // Status is the service status
 type Status string
 
@@ -98,7 +95,7 @@ func GetOrCreateService(name string, timeout uint64) *Service {
 	if services[name] != nil {
 		return services[name]
 	}
-	service := &Service{name, timeout, make(chan uint64), false}
+	service := &Service{name, timeout, make(chan uint64, 1), false}
 
 	services[name] = service
 	return service
@@ -107,17 +104,16 @@ func GetOrCreateService(name string, timeout uint64) *Service {
 // HandleServiceState up the service if down or set timeout for downing the service
 func (service *Service) HandleServiceState(cli *client.Client) (string, error) {
 	status, err := service.getStatus(cli)
+	ctx := context.Background()
+
 	if err != nil {
 		return "", err
 	}
 	if status == UP {
 		fmt.Printf("- Service %v is up\n", service.name)
 		if !service.isHandled {
-			go service.stopAfterTimeout(cli)
-		}
-		select {
-		case service.time <- service.timeout:
-		default:
+			service.time <- service.timeout
+			go service.stopAfterTimeout(ctx, cli)
 		}
 		return "started", nil
 	} else if status == STARTING {
@@ -125,11 +121,11 @@ func (service *Service) HandleServiceState(cli *client.Client) (string, error) {
 		if err != nil {
 			return "", err
 		}
-		go service.stopAfterTimeout(cli)
+		go service.stopAfterTimeout(ctx, cli)
 		return "starting", nil
 	} else if status == DOWN {
 		fmt.Printf("- Service %v is down\n", service.name)
-		service.start(cli)
+		service.start(ctx, cli)
 		return "starting", nil
 	} else {
 		fmt.Printf("- Service %v status is unknown\n", service.name)
@@ -141,28 +137,30 @@ func (service *Service) HandleServiceState(cli *client.Client) (string, error) {
 }
 
 func (service *Service) getStatus(client *client.Client) (Status, error) {
-	ctx := context.Background()
-	dockerContainer, err := service.getDockerContainer(ctx, client)
+	dockerContainer, err := service.getDockerContainer(context.Background(), client)
 
 	if err != nil {
-		return "", err
+		return UNKNOWN, err
 	}
+	
+	if  dockerContainer.State == "restarting" || dockerContainer.State == "starting" {
+		return STARTING, nil
+	} else if dockerContainer.State == "running" {
+		return UP, nil
+	} 
 
-	if dockerContainer.State != "running" {
-		return DOWN, nil
-	}
-	return UP, nil
+	return DOWN, nil
 }
 
-func (service *Service) start(client *client.Client) {
+func (service *Service) start(ctx context.Context, client *client.Client) {
 	fmt.Printf("Starting service %s\n", service.name)
 	service.isHandled = true
-	service.startContainer(client)
-	go service.stopAfterTimeout(client)
+	service.startContainer(ctx, client)
 	service.time <- service.timeout
+	go service.stopAfterTimeout(ctx, client)
 }
 
-func (service *Service) stopAfterTimeout(client *client.Client) {
+func (service *Service) stopAfterTimeout(ctx context.Context, client *client.Client) {
 	service.isHandled = true
 	for {
 		select {
@@ -174,14 +172,13 @@ func (service *Service) stopAfterTimeout(client *client.Client) {
 			}
 		default:
 			fmt.Printf("Stopping service %s\n", service.name)
-			service.stopContainer(client)
+			service.stopContainer(ctx, client)
 			return
 		}
 	}
 }
 
-func (service *Service) stopContainer(client *client.Client) error {
-	ctx := context.Background()
+func (service *Service) stopContainer(ctx context.Context, client *client.Client) error {
 	dockerContainer, err := service.getDockerContainer(ctx, client)
 	if err != nil {
 		return err
@@ -189,50 +186,31 @@ func (service *Service) stopContainer(client *client.Client) error {
 	
 	client.ContainerStop(ctx, dockerContainer.ID, nil)
 	return nil
-
 }
 
-func (service *Service) startContainer(client *client.Client) error {
-	ctx := context.Background()
+func (service *Service) startContainer(ctx context.Context, client *client.Client) error {
 	dockerContainer, err := service.getDockerContainer(ctx, client)
 	if err != nil {
 		return err
 	}
 	
-	client.ContainerStart (ctx, dockerContainer.ID, types.ContainerStartOptions{})
+	client.ContainerStart(ctx, dockerContainer.ID, types.ContainerStartOptions{})
 	return nil
 
 }
 
 func (service *Service) getDockerContainer(ctx context.Context, client *client.Client) (*types.Container, error) {
-	containers, err := client.ContainerList(context.Background(),types.ContainerListOptions{
-		All:     true})
+	containers, err := client.ContainerList(ctx,types.ContainerListOptions{All: true})
 
 	if err != nil {
 		return nil, err
 	}
 
-	/*
 	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.Names[0], container.Image)
-	}
-	*/
-
-	dockerContainer, err := findContainerByName(containers, service.name)
-
-	if err != nil {
-		return nil, err
-	}
-
-
-	return dockerContainer, nil
-}
-
-func findContainerByName(containers []types.Container, name string) (*types.Container, error) {
-	for _, container := range containers {
-		if "/" + name == container.Names[0] {
+		if "/" + service.name == container.Names[0] {
 			return &container, nil
 		}
 	}
-	return &types.Container{}, fmt.Errorf("Could not find service %s", name)
+
+	return nil, fmt.Errorf("Could not find container with name '%s'", service.name)
 }
